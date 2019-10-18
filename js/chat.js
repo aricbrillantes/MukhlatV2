8499e22596182e00fd32cbd9ec94f78a643a6e0d
Rtanchuan <34294661+Rtanchuan@users.noreply.github.com>
2019-10-18 15:10:49 +0800

    comments

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+    // refreshes the function per 1 sec
     setInterval(function(){get_chatmessages()},1000)
     setInterval(function(){get_chatname()},1000)
     // function get_chats()
@@ -22,7 +23,7 @@ $(document).ready(function()
     // }
     // get_chats();
     
-
+    // gets chat messages 
     function get_chatmessages()
     {
         // alert('1');
@@ -30,11 +31,12 @@ $(document).ready(function()
             type: "POST",
             url: base_url + "chat/ajax_getChatMessages",
             // dataType: "json",
+            // chat_id comes from viewchat and passes it as chat_id to the url which is a function
             data: { chat_id : chat_id },
             success: function (data) {
                 // alert('2');
                 
-                   
+                //    since what we return from the function in url is a string we can directly add it as html
                 $("div#chat_viewport").html(data);
                 // $(div).find('#chat_viewport').html('<h1>Hello</h1>');
                 
@@ -42,7 +44,7 @@ $(document).ready(function()
             }
         });
     }
-
+    // same as chatmessages but changes the name of the chat
     function get_chatname()
     {
         // alert('1');
@@ -65,6 +67,7 @@ $(document).ready(function()
     get_chatname();
     get_chatmessages();
 
+    // on click of send button in viewchat
     $("a#submit_msg").click(function(){
         var chat_message = $("input#chat_msg").val();
         
@@ -79,6 +82,7 @@ $(document).ready(function()
             $.ajax({
                 type: "POST",
                 url: base_url + "chat/ajax_add_chat_message",
+                // chat_id and sender_id values come from viewchat while chat_message is local
                 data: { chat_message : chat_message, chat_id : chat_id , sender_id : sender_id },
                 success: function () {
                     // alert('hello');
