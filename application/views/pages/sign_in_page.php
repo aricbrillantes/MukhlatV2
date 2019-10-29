c7c9e10c0c245435782c980f7631e3c662e54347
Aric Brillantes <aric_brillantes@dlsu.edu.ph>
2019-10-29 20:16:12 +0800

    new sign in page

--- application/views/pages/sign_in_page.php
+++ application/views/pages/sign_in_page.php
@@ -14,75 +14,97 @@
 <!-- browser tab icon -->
 <link rel="icon" href="<?php echo base_url('./images/logo/mukhlatlogo_icon.png'); ?>" sizes="32x32"> 
 <link rel="stylesheet" href="<?php echo base_url("/css/style.css"); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<body class = "sign-in">
-    <div class = "container-fluid">
+<body id = "mainbody" class = "sign-in" style="background: #bfcfbf">
+    <div class = "container-fluid ">
         <!-- Logo -->
-        <div class = "row sign-in-logo"><img src = "<?php echo base_url('images/logo/mukhlatlogo1.png'); ?>"></div>
+        <div class = "row sign-in-logo"><img src = "<?php echo base_url('images/logo/mukhlatlogo1.png'); ?>" style="max-width: 30%; height: auto;"></div>
 
         <!-- Content -->
-        <div class = "row sign-in-content">
+        <div id="sign-in-div" class = "row sign-in-content content-container container-fluid"  style="width:80% ">
             <!--Sign In-->
-            <div class = "col-md-10 col-md-offset-1" style = "margin-bottom: 30px;">
+            <div class = "col-md-12 col-xs-12 col-md-offset-0 container-fluid" style = "margin-bottom: 30px;">
                 <div id = "sign-in-container" class = "col-md-12 content-container no-padding">
-                    <form class = "form-inline" id = "log-in-form" onsubmit = "return log_in()" method = "post">
-                        <div class ="form-group">
-                            <h3 class = "sign-in-header no-padding no-margin" style = "margin-left: 40px; padding: 10px;"><strong>Log In</strong></h3>
+                    <div class ="form-group">
+                            <h3 class = "sign-in-header no-padding no-margin" style = "padding: 10px;"><strong id="login" >Hello! Welcome to Mukhlat!</strong></h3>
                         </div>
-                        <div class = "pull-right" style = "padding-top: 10px; padding-right: 10px; padding-bottom: 10px">
-                            <div class = "form-group" style = "margin-right: 5px;">
-                                <input id = "log-in-email" type = "text" required name = "log_in_email" class = "form-control sign-in-field" placeholder = "Email"/>
+
+                    <form class = "form-inline text-center" id = "log-in-form" onsubmit = "return log_in()" method = "post">
+                        
+                        <div class = "row" style = "">
+                            <div class = "form-group content-container container-fluid" style = "">
+                                <input id = "log-in-email" type = "text" required name = "log_in_email" class = "form-control sign-in-field col-md-6 col-sm-12 col-xs-12" placeholder = "Email"/>
                             </div>
-                            <div class = "form-group" style = "margin-right: 5px;">
-                                <input id = "log-in-password" type = "password" required name = "log_in_password"  class = "form-control sign-in-field" placeholder = "Password"/>
+                            <div class = "form-group content-container container-fluid" style = "">
+                                <input id = "log-in-password" type = "password" required name = "log_in_password"  class = "form-control sign-in-field col-md-6 col-sm-12col-xs-12" placeholder = "Password"/>
                             </div>
-                            <div class = "form-group text-center">
-                                <button type="submit" class="btn btn-primary buttonsgo" id="loginbutton" style = "width: 100%;font-size:24px;">Login</button>
+                            
+                            <div class = "form-group text-center content-container container-fluid">
+                                <button type="submit" class="btn btn-primary buttonsgo col-md-2 col-sm-12 col-xs-12"  id="loginbutton" style = "width: 100%;font-size:24px;">Login</button>
                             </div>
                         </div>
                     </form>
                 </div>
+                <div id="change-sign-in-1" class="text-center" style="margin-top: 0">
+                    <button onclick="changeUI()" style="background-color: Transparent;background-repeat:no-repeat;border: none;cursor:pointer;overflow: hidden;outline:none;color:black"><br>Are you a parent? Click here!</button>
+                </div>
+            </div>
+
+            <div id="change-sign-in-2" class="text-center" style="margin-top: 150px;  display: none">
+                <button onclick="changeUI()" style="background-color: Transparent;background-repeat:no-repeat;border: none;cursor:pointer;overflow: hidden;outline:none;color:black ">Are you a child? Click here!</button>
             </div>
 
             <!--Registration-->
-            <div class = "col-md-10 col-md-offset-1">
+            <div class = "col-md-10 col-md-offset-1" id="sign-up" style="display:none; margin-top: 25px" >
+
                 <div id = "sign-up-container" class = "col-md-12 content-container no-padding">
                     <div class = "col-md-12 sign-in-div">
                         <h3 class = "sign-in-header"><strong>Sign Up for Mukhlat!</strong></h3>
                         <div class = "sign-in-form">
                             <form id = "sign-up-form" onsubmit = "return sign_up()" method = "post">
-                                <div class = "col-xs-6 form-group register-field">
-                                    <input type = "text" required name = "first_name" class = "form-control sign-in-field" placeholder = "First Name" maxlength = "25">
+
+                                <div class = "col-xs-12 col-sm-2 col-md-12 form-group register-field content-container container-fluid" >
+                                    <div class = "col-xs-12 col-sm-2 col-md-1 form-group register-field content-container container-fluid" style="margin-right: 25px">
+                                        <p class = "text-muted"><strong>Role: </strong></p>
+                                    </div>
+
+                                    <div class = "col-xs-12 col-md-3 form-group register-field content-container container-fluid" >
+                                        <select class = "form-control" name = "sign_up_role" id = "sign_up_role">
+                                            <option value="2">Child</option>
+                                            <option value="1">Administrator</option>
+                                            <option value="3">Parent</option>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div class = "col-xs-6 form-group register-field">
-                                    <input type = "text" required name = "last_name" class = "form-control sign-in-field" placeholder = "Last Name" maxlength = "25">
+
+                                <div class = "col-xs-12 col-md-6 form-group content-container container-fluid" style = "">
+                                    <input type = "text" required name = "first_name" class = "form-control sign-in-field col-md-4 col-md-offset-0 " placeholder = "First Name" maxlength = "25">
                                 </div>
-                                <div class = "col-xs-12 form-group register-field">
-                                    <input type = "email" required name = "sign_up_email" class = "form-control sign-in-field" placeholder = "Email Address" maxlength = "45">
+                                <div class = "col-xs-12 col-md-6 form-group register-field content-container container-fluid">
+                                    <input type = "text" required name = "last_name" class = "form-control sign-in-field col-md-4 col-md-offset-0" placeholder = "Last Name" maxlength = "25">
                                 </div>
-                                <div class = "col-xs-2 text-center register-field">
-                                    <h5 class = "text-muted"><strong>Birthday: </strong></h5>
+                                <div class = "col-xs-12 col-md-6 form-group register-field content-container container-fluid">
+                                    <input type = "email" required id = "sign_up_email" name = "sign_up_email" class = "form-control sign-in-field col-md-4 col-md-offset-0" placeholder = "Email Address" maxlength = "45">
                                 </div>
-                                <div class = "col-xs-4 form-group register-field">
-                                    <input type = "date" required name = "sign_up_birthday" class = "form-control sign-in-field">
+                                <div class = "col-xs-12 col-sm-auto col-md-2 form-group register-field content-container container-fluid">
+                                    <p class = "text-muted"><strong>Birthday: </strong></p>
                                 </div>
-                                <div class = "col-xs-1 text-center register-field">
-                                    <h5 class = "text-muted"><strong>Role: </strong></h5>
+                                <div class = "col-xs-12 col-sm-auto col-md-4 form-group register-field content-container container-fluid">
+                                    <input type = "date" required name = "sign_up_birthday" class = "form-control sign-in-field col-md-4 col-md-offset-0">
                                 </div>
-                                <div class = "col-xs-5 form-group register-field">
-                                    <select class = "form-control" name = "sign_up_role">
-                                        <?php foreach ($roles as $role) : ?>
-                                            <option value="<?php echo $role->role_id; ?>"><?php echo $role->role_name; ?></option>
-                                        <?php endforeach; ?>
-                                    </select>
+                                <div class = "col-xs-12 col-md-6 form-group register-field content-container container-fluid">
+                                    <input id = "sign-up-password" type = "password" required name = "sign_up_password" class = "form-control sign-in-field col-md-6 col-md-offset-0" placeholder = "Password">
                                 </div>
-                                <div class = "password-field col-xs-6 form-group register-field">
-                                    <input id = "sign-up-password" type = "password" required name = "sign_up_password" class = "form-control sign-in-field" placeholder = "Password">
+                                <div class = "col-xs-12 col-md-6 form-group register-field content-container container-fluid">
+                                    <input id = "sign-up-retype" type = "password" required class = "form-control sign-in-field col-md-4 col-md-offset-0" placeholder = "Retype Password">
                                 </div>
-                                <div class = "password-field col-xs-6 form-group register-field">
-                                    <input id = "sign-up-retype" type = "password" required class = "form-control sign-in-field" placeholder = "Retype Password">
-                                </div>
-                                <div class = "text-center">
+
+                                <div id="parent-email" class = "col-xs-12 col-md-6 form-group register-field content-container container-fluid" style="visibility: visible">
+                                    <input type = "email" id = "sign_up_email_parent" name = "sign_up_email_parent" class = "form-control sign-in-field col-md-4 col-md-offset-0" placeholder = "Parent' Email Address" maxlength = "45">
+                                </div><br>
+                                                           
+                                <div class = "text-center col-xs-12 col-md-12 form-group content-container container-fluid">
                                     <button type = "submit" class = "btn btn-success" style="width:100%;">Register</button>
                                 </div>
                             </form>
@@ -90,9 +112,75 @@
                     </div>
                 </div>
             </div>
+            
         </div>
     </div>
 
-    <script type="text/javascript" src="<?php echo base_url("/js/sign_in.js"); ?>"></script>
+
+    <style type="text/css">
+        .content-container
+        {
+            border: 0px;
+        }
+
+        button
+        {
+            box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+        }
+
+    </style>
+
 </body>
+
+<script type="text/javascript" src="<?php echo base_url("/js/sign_in.js"); ?>"></script>
+
+<script>
+
+    document.getElementById('sign_up_role').onchange = function() 
+    {
+        // alert(this.value);
+        if(this.value === "2")
+        {
+            document.getElementById("parent-email").style.visibility = "visible";
+            document.getElementById("sign_up_email_parent").value = document.getElementById("sign_up_email").value;
+        }
+
+        else
+            document.getElementById("parent-email").style.visibility = "hidden";
+    };
+
+    var toggle = 0;
+
+    function changeUI() 
+    {
+        var background = document.getElementById("mainbody").style.background;
+        // alert(background);
+        
+        if (toggle==1) 
+        {
+            document.getElementById("mainbody").style.background = "#bfcfbf";
+            document.getElementById("sign-up").style.display = "none";
+            document.getElementById("change-sign-in-2").style.display = "none";
+            document.getElementById("change-sign-in-1").style.display = "block";
+            document.getElementById("login").innerHTML = "Hello! Welcome to Mukhlat!";
+            toggle=0;
+        } 
+
+        else 
+        {
+            document.getElementById("mainbody").style.background = "#f2f2f2";
+            document.getElementById("sign-up").style.display = "block";
+            document.getElementById("change-sign-in-1").style.display = "none";
+            document.getElementById("change-sign-in-2").style.display = "block";
+            document.getElementById("login").innerHTML = "Log In";
+            toggle=1;
+        }
+
+    }
+
+</script>
+
+
+
 </html>
+
