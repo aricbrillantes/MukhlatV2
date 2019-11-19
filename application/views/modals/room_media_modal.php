0636628ee2179ac718b01655aec2396e87d6abde
ComSciStudent <32064740+ComSciStudent@users.noreply.github.com>
2019-11-19 15:34:51 +0800

    my stuff modal update

--- application/views/modals/room_media_modal.php
+++ application/views/modals/room_media_modal.php
@@ -28,22 +28,43 @@
                                                     if ($attachment->attachment_type_id === '1'):?>
                                                         <div class="col-md-3 <?php echo $theme?> polaroiditem polaroid homepostsborder" style="margin-right:1%;margin-bottom:1%">
                                                             <div class="whitebg">
+                                                            <img class = "img-circle" style = "margin: 10px 0px;" width = "40px" height = "40px" src = "<?php echo $post->user->profile_url ? base_url($post->user->profile_url) : base_url('images/default.jpg'); ?>"/>
+                                                            <small><a href="<?php echo base_url('user/profile/' . $post->user_id); ?>"><?php echo $post->user->first_name;?></a></small>
+                                                            <br>
+                                                            <span class = "text-muted"> <i style = "font-size: 18px;padding-left: 10px"><?php echo date("F d, Y", strtotime($post->date_posted)); ?></i></span>
+                                                            <hr>
+                                                            <div class=" polaroidwrapper">
                                                                 <p><?php echo utf8_decode($post->post_content); ?></p>
                                                                 <img src = "<?= base_url($attachment->file_url); ?>" width = "75%"  style="position:relative;" />
+                                                            </div>
                                                             </div>
                                                         </div>
                                                 <?php elseif ($attachment->attachment_type_id === '2'):?>
                                                         <div class="col-md-3 <?php echo $theme?> polaroiditem polaroid homepostsborder" style="margin-right:1%;margin-bottom:1%">
                                                             <div class="whitebg">
+                                                            <img class = "img-circle" style = "margin: 10px 0px;" width = "40px" height = "40px" src = "<?php echo $post->user->profile_url ? base_url($post->user->profile_url) : base_url('images/default.jpg'); ?>"/>
+                                                            <small><a href="<?php echo base_url('user/profile/' . $post->user_id); ?>"><?php echo $post->user->first_name;?></a></small>
+                                                            <br>
+                                                            <span class = "text-muted"> <i style = "font-size: 18px;padding-left: 10px"><?php echo date("F d, Y", strtotime($post->date_posted)); ?></i></span>
+                                                            <hr>
+                                                            <div class=" polaroidwrapper">
                                                                 <p><?php echo utf8_decode($post->post_content); ?></p>
                                                                 <audio src = "<?= base_url($attachment->file_url); ?>" controls></audio>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                 <?php elseif ($attachment->attachment_type_id === '3'):?>
                                                         <div class="col-md-3 <?php echo $theme?> polaroiditem polaroid homepostsborder" style="margin-right:1%;margin-bottom:1%">
                                                             <div class="whitebg">
+                                                            <img class = "img-circle" style = "margin: 10px 0px;" width = "40px" height = "40px" src = "<?php echo $post->user->profile_url ? base_url($post->user->profile_url) : base_url('images/default.jpg'); ?>"/>
+                                                            <small><a href="<?php echo base_url('user/profile/' . $post->user_id); ?>"><?php echo $post->user->first_name;?></a></small>
+                                                            <br>
+                                                            <span class = "text-muted"> <i style = "font-size: 18px;padding-left: 10px"><?php echo date("F d, Y", strtotime($post->date_posted)); ?></i></span>
+                                                            <hr>
+                                                            <div class=" polaroidwrapper">
                                                                 <p><?php echo utf8_decode($post->post_content); ?></p>
                                                                 <video src = "<?= base_url($attachment->file_url); ?>" width = "300px" controls/></video>
+                                                            </div>
                                                             </div>
                                                         </div>
                     
