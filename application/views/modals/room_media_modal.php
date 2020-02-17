7cfa89f7927c6848458b5b16bf0656fa541f4231
ComSciStudent <32064740+ComSciStudent@users.noreply.github.com>
2020-02-17 12:21:55 +0800

    room media modal minor fix

--- application/views/modals/room_media_modal.php
+++ application/views/modals/room_media_modal.php
@@ -147,15 +147,16 @@
                       var i;
                       var slides = document.getElementsByClassName("mySlides");
                       var dots = document.getElementsByClassName("dot");
-                      if (n > slides.length) {slideIndex = 1}    
-                      if (n < 1) {slideIndex = slides.length}
+                      if (n > slides.length) {slideIndex = 1;}    
+                      if (n < 1) {slideIndex = slides.length;}
                       for (i = 0; i < slides.length; i++) {
                         slides[i].style.display = "none";  
                       }
                       for (i = 0; i < dots.length; i++) {
                           dots[i].className = dots[i].className.replace(" selected3", "");
                       }
-                      slides[slideIndex-1].style.display = "block";  
+                      if(slides[slideIndex-1] !== null)
+                        slides[slideIndex-1].style.display = "block";   
                       dots[slideIndex-1].className += " selected3";  
                     //  slideIndex++;
                     //  if (slideIndex > slides.length) {slideIndex = 1};
