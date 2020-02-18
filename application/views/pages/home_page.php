c2d928fb6fb2533fea0286b0b2099dfd5db8adbf
ComSciStudent <32064740+ComSciStudent@users.noreply.github.com>
2020-02-19 07:32:03 +0800

    minor polish

--- application/views/pages/home_page.php
+++ application/views/pages/home_page.php
@@ -145,7 +145,7 @@
                                                 <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>"> your room, </a>
 
                                             <?php else:?>
-                                                <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>"> <?php echo utf8_decode($post->topic_name); ?>, </a>
+                                                <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>"> <?php echo utf8_decode($post->topic_name); ?>'s Room, </a>
                                             
                                             <?php endif;?>
                                             <a class = "text1color" href = "<?php echo base_url('user/profile/' . $post->user_id); ?>">
