897443c3e41d4dfcf7d37d10089b5e729730315b
ComSciStudent <32064740+ComSciStudent@users.noreply.github.com>
2020-02-26 12:47:16 +0800

    decorated to posted

--- application/views/pages/home_page.php
+++ application/views/pages/home_page.php
@@ -117,7 +117,7 @@
                                                     <strong class="clicker" style = "font-size: 21px;">You</strong>
                                                 </a> 
 
-                                                <p style="font-size: 24px; display:inline"> decorated </p>
+                                                <p style="font-size: 24px; display:inline"> posted </p>
                                                 <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>">your room! </a>
 
                                             <?php elseif($post->user_id !== $logged_user->user_id): ?>
@@ -135,7 +135,7 @@
                                             <a class = "text1color" href = "<?php echo base_url('user/profile/' . $post->user_id); ?>">
                                                 <strong class="clicker" style = "font-size: 21px"><?php echo $post->first_name;?></strong>
                                             </a>
-                                            <p style="font-size: 24px; display:inline"> decorated </p> 
+                                            <p style="font-size: 24px; display:inline"> posted </p> 
                                             <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>">their room! </a>
 
                                         <?php else: ?>
@@ -145,7 +145,7 @@
                                                 <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>"> your room, </a>
 
                                             <?php else:?>
-                                                <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>"> <?php echo utf8_decode($post->topic_name); ?>'s Room, </a>
+                                                <a style="max-width: 500px; overflow: hidden; text-overflow: ellipsis; display:inline " class = "" href = "<?php echo base_url('topic/view/' . $post->topic_id); ?>"> <?php echo utf8_decode($post->user_id); ?>'s Room, </a>
                                             
                                             <?php endif;?>
                                             <a class = "text1color" href = "<?php echo base_url('user/profile/' . $post->user_id); ?>">
